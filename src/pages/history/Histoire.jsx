b0ab378fc61778abc4b278dfe8389cdc08c28f95
Baboo81 <magicbaboo@gmail.com>
2023-02-20 16:31:37 +0100

    Added Txt

--- src/pages/history/Histoire.jsx
+++ src/pages/history/Histoire.jsx
@@ -6,6 +6,11 @@ const Histoire = () => {
     return (
         <div>
             <h2>Un peu d'histoire</h2>
+            <section>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores eligendi minus, tempora, est cumque numquam incidunt magni qui laudantium saepe accusantium commodi dicta laboriosam labore, voluptates adipisci. Neque cum numquam ab qui ad illum inventore. Aliquam necessitatibus quasi error natus, ex nisi adipisci maiores quia excepturi. Ratione similique est repellat vero omnis obcaecati id maxime. Vel voluptatem repellat blanditiis. Quibusdam, quam consectetur ad voluptates nam magni obcaecati aperiam, in illo sapiente expedita autem doloremque libero accusantium. Harum cumque deserunt aut beatae quaerat numquam, voluptatem molestias in eum veritatis, perferendis possimus facere eveniet ea est a impedit accusamus, sed esse voluptatibus.
+                </p>
+            </section>
         </div>
     )
 }
