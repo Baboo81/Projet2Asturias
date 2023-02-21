aea099dc9ad33e5f2fae642ba952e9eb1fa87471
Baboo81 <magicbaboo@gmail.com>
2023-02-21 20:05:04 +0100

    Added component

---
+++ src/components/AnimateTitle.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { animation } from "react-reveal/globals";
+import Zoom from "react-reveal/Zoom";
+import Bounce from "react-reveal/Bounce";
+import Roll from "react-reveal/Roll";
+import Flip from "react-reveal/Flip";
+
+const AnimateTitle = () => {
+    state = { animation : "zoom" };
+
+    handleAnimation = animation => {
+        this.setState ({ animation });
+    };
+
+    getText = () => {
+        return(
+            <div
+                style = {{
+                    marginTop : "150px",
+                    fontSize : "25px",
+                    color : "teal",
+                    fontWeight : "bold"
+                }}
+            >
+                Hello
+            </div>
+        );
+    };
+
+    render() {
+        let comp;
+    switch (this.state.animation) {
+      case "flip":
+        comp = <Flip>{this.getText()}</Flip>;
+        break;
+      case "zoom":
+        comp = <Zoom>{this.getText()}</Zoom>;
+        break;
+      case "bounce":
+        comp = <Bounce>{this.getText()}</Bounce>;
+        break;
+      case "roll":
+        comp = <Roll>{this.getText()}</Roll>;
+        break;
+      default:
+        comp = <Zoom>{this.getText()}</Zoom>;
+
+    }
+
+    return (
+
+    )
+}
+
+export default AnimateTitle;
